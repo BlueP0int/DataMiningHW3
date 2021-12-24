fa8a2658195f06133fb95b81fa5cfcdcbda7b30b
blueshit007 <773888678@qq.com>
2021-12-24 23:00:33 +0800

    update result

--- creditCardClustering.py
+++ creditCardClustering.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 from numpy import genfromtxt
-from sklearn.cluster import kmeans_plusplus
-from sklearn.cluster import KMeans
+# from sklearn.cluster import kmeans_plusplus
+# from sklearn.cluster import KMeans
 from sklearn.cluster import AffinityPropagation
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
@@ -23,11 +23,13 @@ from numpy.random import uniform
 import numpy as np
 from math import isnan
 
-standedColorList = ['aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure', 'beige', 'bisque', 'black', 'blanchedalmond', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgreen', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'greenyellow', 'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgreen', 'lightgray', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen', 'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab', 'orange', 'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple', 'red', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'snow', 'springgreen', 'steelblue', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'white', 'whitesmoke', 'yellow', 'yellowgreen', ]
+# standedColorList = ['aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure', 'beige', 'bisque', 'black', 'blanchedalmond', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgreen', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'greenyellow', 'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgreen', 'lightgray', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen', 'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab', 'orange', 'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple', 'red', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'snow', 'springgreen', 'steelblue', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'white', 'whitesmoke', 'yellow', 'yellowgreen', ]
 
+standedColorList = ['red','blue','orangered','green','darkmagenta', 'purple','pink']
 
-
- 
+#  应用霍普金斯统计量（Hopkins Statistic） 可以判断数据在空间上的随机性，从而判断数据是否可以聚类
+#  如果样本接近随机分布，H的值接近于0.5；
+# 如果聚类趋势明显，则随机生成的样本点距离应该远大于实际样本点的距离H的值接近于1.
 def hopkins(X):
     X = pd.DataFrame(X)
     d = X.shape[1]
@@ -57,7 +59,7 @@ def hopkins(X):
 def generatePCAMap(labels,embeddings,embeddingFileName):
     colorList = []
     for item in labels:
-        colorList.append(standedColorList[5*int(item)%len(standedColorList)])
+        colorList.append(standedColorList[int(item)%len(standedColorList)])
         # if(int(item) == 0):
         #     colorList.append('m')
         # else:
@@ -71,11 +73,11 @@ def generatePCAMap(labels,embeddings,embeddingFileName):
 
     plt.title(embeddingFileName)
 
-    plt.savefig(embeddingFileName + '.jpg', format='jpg', dpi=200)
+    plt.savefig(embeddingFileName + '.jpg', format='jpg', dpi=300)
     print('saved '+ embeddingFileName + '.jpg')
 
 def sigmoid(z):
-    return 10 / (1.0 + np.exp(-z))
+    return 1/ (1.0 + np.exp(-z))
 
 def loaddata(filename):   
     X = pd.read_csv('CCGENERAL.csv')
@@ -84,56 +86,66 @@ def loaddata(filename):
     return X, idLabels
 
 def datapreprocess(X):
-    X[np.isnan(X)]=0
-    print(X.shape)
+    # X[np.isnan(X)]=0    
+    # 使用均值替换nan值
+    col_mean = np.nanmean(X, axis=0)
+    inds = np.where(np.isnan(X))
+    X[inds] = np.take(col_mean, inds[1])
     
-    X = sigmoid(X) 
-    
+    # print(X.shape)    
+    X = sigmoid(X)     
     X = StandardScaler().fit_transform(X)
     return X
     
+    
+def evaluate(X, clustering, modelName):
+    # 轮廓系数（Silhouette Coefficient）结合了聚类的凝聚度（Cohesion）和分离度（Separation），用于评估聚类的效果。该值处于-1~1之间，值越大，表示聚类效果越好。
+    # Silhouette Coefficient or silhouette score is a metric used to calculate the goodness of a clustering technique.
+    # Its value ranges from -1 to 1.
+    # 1: Means clusters are well apart from each other and clearly distinguished.
+    # 0: Means clusters are indifferent, or we can say that the distance between clusters is not significant.
+    # -1: Means clusters are assigned in the wrong way.
+    # Calinski-Harabasz指标通过计算类中各点与类中心的距离平方和来度量类内的紧密度，通过计算各类中心点与数据集中心点距离平方和来度量数据集的分离度，CH指标由分离度与紧密度的比值得到。从而，CH越大代表着类自身越紧密，类与类之间越分散，即更优的聚类结果。
+    Silhouette_Coefficient = metrics.silhouette_score(X, clustering, metric='euclidean')
+    CalinskiHarabasz_index = metrics.calinski_harabasz_score(X, clustering)
+    # print(Silhouette_Coefficient,CalinskiHarabasz_index)
+    print("{} Silhouette_Coefficient is {},CalinskiHarabasz_index is {}".format(modelName,Silhouette_Coefficient,CalinskiHarabasz_index))
+    with open("log.txt",'a') as f:
+        f.writelines("| {} | {:.3f} |{:.3f} |\n".format(modelName,Silhouette_Coefficient,CalinskiHarabasz_index))
+    
+    
+    
 def main():
-    # X = pd.read_csv('CCGENERAL.csv')
-    # idLabels = X['CUST_ID'].values
-    # X = X.drop(labels='CUST_ID', axis=1).values
-    # X[np.isnan(X)]=0
-    # print(X.shape)
-    
-    # X = sigmoid(X) 
-    
-    # X = StandardScaler().fit_transform(X)
-    
-    
     X, idLabels = loaddata('CCGENERAL.csv')
     X = datapreprocess(X)
-    print(hopkins(X))
+    print("Hopkins Statistic is: {}".format(hopkins(X)))
     
-    y_pred = KMeans(n_clusters=4, random_state=42).fit_predict(X)
-    generatePCAMap(y_pred,X,"KMeans")    
+    # with open("log.txt",'a') as f:
+    #     f.writelines("\n\n### Table Clustering Evaluation\n")
+    #     f.writelines("| ModelName | Silhouette_Coefficient | CalinskiHarabasz_index |\n")
+        
+    # clustering = AffinityPropagation(random_state=4).fit(X)
+    # generatePCAMap(clustering.labels_, X, "AffinityPropagation")
+    # evaluate(X, clustering.labels_, "AffinityPropagation")
     
-    clustering = AffinityPropagation(random_state=4).fit(X)
-    generatePCAMap(clustering.labels_, X, "AffinityPropagation")
+    # clustering = DBSCAN(eps=0.3, min_samples=4).fit(X)
+    # generatePCAMap(clustering.labels_, X, "DBSCAN")
+    # evaluate(X, clustering.labels_, "DBSCAN")
     
-    clustering = DBSCAN(eps=0.3, min_samples=4).fit(X)
-    generatePCAMap(clustering.labels_, X, "DBSCAN")
+    # clustering = AgglomerativeClustering(n_clusters=4, linkage="ward").fit(X)
+    # generatePCAMap(clustering.labels_, X, "Agglomerative")
+    # evaluate(X, clustering.labels_, "Agglomerative")
     
-    clustering = AgglomerativeClustering(n_clusters=4, linkage="ward").fit(X)
-    generatePCAMap(clustering.labels_, X, "Agglomerative")
+    # bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=100)
+    # clustering = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+    # clustering.fit(X)
+    # generatePCAMap(clustering.labels_, X, "MeanShift")
+    # evaluate(X, clustering.labels_, "MeanShift")
     
-    clf = Kmeans(k=4)
+    clf = Kmeans(k=3)
     clustering = clf.predict(X)
-    generatePCAMap(clustering, X, "kmeans2")
-    
-    bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=100)
-    clustering = MeanShift(bandwidth=bandwidth, bin_seeding=True)
-    clustering.fit(X)
-    generatePCAMap(clustering.labels_, X, "MeanShift")
-    
-    # 因为没有标签，所以一般通过评估类的分离情况来决定聚类质量。类内越紧密，类间距离越小则质量越高。我用到过的有sklearn中的Silhouette Coefficient和Calinski-Harabaz Index，sklearn里面解释的很清楚，直接把数据和聚类结果作为输入就可以了。
-    Silhouette_Coefficient = metrics.silhouette_score(X, clustering.labels_, metric='euclidean')
-    CalinskiHarabasz_index = metrics.calinski_harabasz_score(X, clustering.labels_)
-    print(Silhouette_Coefficient,CalinskiHarabasz_index)
-
+    generatePCAMap(clustering, X, "kmeans")
+    evaluate(X, clustering, "kmeans")
 
 if __name__ == "__main__":
     main()
--- kmeans.py
+++ kmeans.py
@@ -3,43 +3,17 @@ from sklearn import datasets
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
-
-# 正规化数据集 X
-def normalize(X, axis=-1, p=2):
-    lp_norm = np.atleast_1d(np.linalg.norm(X, p, axis))
-    lp_norm[lp_norm == 0] = 1
-    return X / np.expand_dims(lp_norm, axis)
-
-
-# 计算一个样本与数据集中所有样本的欧氏距离的平方
-def euclidean_distance(one_sample, X):
-    one_sample = one_sample.reshape(1, -1)
-    X = X.reshape(X.shape[0], -1)
-    distances = np.power(np.tile(one_sample, (X.shape[0], 1)) - X, 2).sum(axis=1)
-    return distances
-
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 
 class Kmeans():
-    """Kmeans聚类算法.
-
-    Parameters:
-    -----------
-    k: int
-        聚类的数目.
-    max_iterations: int
-        最大迭代次数. 
-    varepsilon: float
-        判断是否收敛, 如果上一次的所有k个聚类中心与本次的所有k个聚类中心的差都小于varepsilon, 
-        则说明算法已经收敛
-    """
-    def __init__(self, k=2, max_iterations=500, varepsilon=0.0001):
+    def __init__(self, k=2, max_iterations=2000, varepsilon=0.0001):
         self.k = k
         self.max_iterations = max_iterations
         self.varepsilon = varepsilon
 
-    # 从所有样本中随机选取self.k样本作为初始的聚类中心
+    # 随机选取k个样本作为初始聚类中心
     def init_random_centroids(self, X):
         n_samples, n_features = np.shape(X)
         centroids = np.zeros((self.k, n_features))
@@ -48,9 +22,16 @@ class Kmeans():
             centroids[i] = centroid
         return centroids
     
+    # 计算欧氏距离的平方
+    def euclidean_distance(self, one_sample, X):
+        one_sample = one_sample.reshape(1, -1)
+        X = X.reshape(X.shape[0], -1)
+        distances = np.power(np.tile(one_sample, (X.shape[0], 1)) - X, 2).sum(axis=1)
+        return distances
+    
     # 返回距离该样本最近的一个中心索引[0, self.k)
     def _closest_centroid(self, sample, centroids):
-        distances = euclidean_distance(sample, centroids)
+        distances = self.euclidean_distance(sample, centroids)
         closest_i = np.argmin(distances)
         return closest_i
 
@@ -63,7 +44,7 @@ class Kmeans():
             clusters[centroid_i].append(sample_i)
         return clusters
 
-    # 对中心进行更新
+    # update中心
     def update_centroids(self, clusters, X):
         n_features = np.shape(X)[1]
         centroids = np.zeros((self.k, n_features))
@@ -82,50 +63,14 @@ class Kmeans():
 
     # 对整个数据集X进行Kmeans聚类，返回其聚类的标签
     def predict(self, X):
-        # 从所有样本中随机选取self.k样本作为初始的聚类中心
         centroids = self.init_random_centroids(X)
 
-        # 迭代，直到算法收敛(上一次的聚类中心和这一次的聚类中心几乎重合)或者达到最大迭代次数
         for _ in range(self.max_iterations):
-            # 将所有进行归类，归类规则就是将该样本归类到与其最近的中心
             clusters = self.create_clusters(centroids, X)
             former_centroids = centroids
-            
-            # 计算新的聚类中心
-            centroids = self.update_centroids(clusters, X)
-            
-            # 如果聚类中心几乎没有变化，说明算法已经收敛，退出迭代
+            centroids = self.update_centroids(clusters, X)            
             diff = centroids - former_centroids
             if diff.any() < self.varepsilon:
                 break
             
         return self.get_cluster_labels(clusters, X)
-
-
-def main():
-    # Load the dataset
-    X, y = datasets.make_blobs(n_samples=10000, 
-                               n_features=3, 
-                               centers=[[3,3, 3], [0,0,0], [1,1,1], [2,2,2]], 
-                               cluster_std=[0.2, 0.1, 0.2, 0.2], 
-                               random_state =9)
-    
-
-    # 用Kmeans算法进行聚类
-    clf = Kmeans(k=4)
-    y_pred = clf.predict(X)
-    
-
-    # 可视化聚类效果
-    fig = plt.figure(figsize=(12, 8))
-    ax = Axes3D(fig, rect=[0, 0, 1, 1], elev=30, azim=20)
-    plt.scatter(X[y==0][:, 0], X[y==0][:, 1], X[y==0][:, 2])
-    plt.scatter(X[y==1][:, 0], X[y==1][:, 1], X[y==1][:, 2])
-    plt.scatter(X[y==2][:, 0], X[y==2][:, 1], X[y==2][:, 2])
-    plt.scatter(X[y==3][:, 0], X[y==3][:, 1], X[y==3][:, 2])
-    plt.show()
-    plt.savefig('kkm.jpg', format='jpg', dpi=1000)
-
-
-if __name__ == "__main__":
-    main()
